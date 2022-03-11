2bffec85322a2eec837836fd982c3cc4a9fdd423
aditya <aditya.n.h02@gmail.com>
2022-03-11 16:09:07 +0700

    menambahkan implementasi dengan state-based construction

--- tpmodul3_1302204078/Program.cs
+++ tpmodul3_1302204078/Program.cs
@@ -8,6 +8,9 @@
             Console.WriteLine("Keluarahan       Kode Pos");
             table_Kodepos.getKodePos();
 
+            Console.WriteLine("====== KunciPintu ======");
+            DoorMachine pintu = new DoorMachine();
+            pintu.kunci();
         }
 
     }
@@ -18,14 +21,14 @@
             {"Batununggal", "40266"},
             {"Kujangsari", "40287"},
             {"Mengger", "40267"},
-            {"Wates", "        40256"},
+            {"Wates", "40256"},
             {"Cijaura", "40287"},
             {"Jatisari", "40286"},
             {"Margasari", "40286"},
             {"Sekejati", "40286"},
             {"Kebonwaru", "40272"},
-            {"Maleer", "        40274"},
-            {"Samoja", "        40273"}
+            {"Maleer", "40274"},
+            {"Samoja", "40273"}
         };
 
         public void getKodePos()
@@ -34,6 +37,37 @@
             {
                 Console.WriteLine("{0} \t {1}", ele1.Key, ele1.Value);
             }
+        }
+    }
+    class DoorMachine
+    {
+        enum State { Terkunci, Terbuka };
+        public void kunci()
+        {
+            State state = State.Terkunci;
+
+            String[] screenName = { "Pintu Terkunci", "Pintu tidak Terkunci" };
+            do
+            {
+                Console.WriteLine("" + screenName[(int)state]);
+                Console.Write("Masukan Command : ");
+                String command = Console.ReadLine();
+                switch (state)
+                {
+                    case State.Terbuka:
+                        if (command != "BukaPintu")
+                        {
+                            state = State.Terkunci;
+                        }
+                        break;
+                    case State.Terkunci:
+                        if (command != "KunciPintu")
+                        {
+                            state = State.Terbuka;
+                        }
+                        break;
+                }
+            } while (state != State.Terkunci);
         }
     }
 }
